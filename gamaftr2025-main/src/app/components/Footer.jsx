f8f265fa593101cda9b36cc31221c87f0737d8b0
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-11-10 15:32:14 +0700

    Update Footer.jsx

--- gamaftr2025-main/src/app/components/Footer.jsx
+++ gamaftr2025-main/src/app/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="flex p-2 md:items-center sm:justify-between gap-[10px] md:gap-[40px] md:p-10 md:rounded-full shadow-2xl footer bg-space2/70 text-space1 shadow-red-700 md:scale-100 md:px-36 relative z-40">
   <aside className='flex'>
   <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/Gamaft/Logo_Institut_Teknologi_Bandung.png?raw=true" alt="" className="max-w-[60px] md:max-w-[150px] md:pl-4 pl-1 ml-3" />
-    <p className='hidden text-sm font-extrabold text-gray-800 lg:block md:pt-7 lg:text-lg font-spacegrotesk'>Gadjah Mada <br className='text-sm'/>Flies To Riau 2024<br />©IT GAMAFTR </p>
+    <p className='hidden text-sm font-extrabold text-gray-800 lg:block md:pt-7 lg:text-lg font-spacegrotesk'>Gadjah Mada <br className='text-sm'/>Flies To Riau 2024<br />AAAAAAAAAA </p>
     <p className='block text-[10px] font-extrabold text-gray-800 lg:hidden md:pt-7 lg:text-lg font-spacegrotesk pt-5'>©AAAAAAAAAAAAAAAAAAAAAAAA </p>
   </aside> 
   <nav className=''>
