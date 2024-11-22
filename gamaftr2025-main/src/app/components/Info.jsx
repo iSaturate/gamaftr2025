835057d39804f482811853969fc044afbc94582b
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-11-22 17:03:52 +0700

    Update Info.jsx

--- gamaftr2025-main/src/app/components/Info.jsx
+++ gamaftr2025-main/src/app/components/Info.jsx
@@ -29,7 +29,7 @@ const Info = () => {
             ></iframe>
           </div>
         </div>
-        <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/image/Tower%202%201.png?raw=true" alt="" className="absolute z-10 translate-x-[248px] xl:translate-y-[-800px] md:translate-y-[-500px] lg:translate-y-[-400px] xl:scale-[75%] md:scale-[50%] sm:scale-[80%] lg:scale-[60%] hidden md:block " />
+        <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/image/Tower%202%201.png?raw=true" alt="" className="absolute z-10 translate-x-[248px] xl:translate-y-[-750px] md:translate-y-[-500px] lg:translate-y-[-400px] xl:scale-[75%] md:scale-[50%] sm:scale-[80%] lg:scale-[60%] hidden md:block " />
         <div className="container relative z-40 w-1/2 p-4 mx-auto overflow-hidden  bg-red-400 border-[10px] shadow-2xl border-space2 shadow-space2 rounded-xl ">
           <h1 className="mb-4 text-xl font-bold text-white font-montserrat md:text-xl sm:text-lg lg:text-4xl xl:text-6xl lg:text-border-space2">Gadjah Mada Flies To Riau</h1>
           <p className="font-semibold leading-relaxed text-justify text-white font-spacegrotesk md:text-md sm:text-sm lg:text-xl">Merupakan program kerja tahunan KEMARIGAMA (Keluarga Mahasiswa Riau Gadjah Mada) yang setiap tahunnya sudah mengunjungi lebih dari 50 SMA di Provinsi Riau. GAMAFTR diinsiasikan untuk memperkenalkan Universitas Gadjah Mada kepada siswa dan siswi SMA/sederajat di Provinsi Riau.</p>
