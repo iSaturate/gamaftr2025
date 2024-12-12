38d0e20cef64d895928dd298671720d7c59526ac
Harum S Astyanti <hasatast@gmail.com>
2024-12-12 22:52:28 +0700

    Update Navbar.jsx

--- gamaftr2025-main/src/app/components/Navbar.jsx
+++ gamaftr2025-main/src/app/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
           <div className="items-center hidden gap-5 space-x-1 md:flex">
             <a href="#event" className="px-6 py-2 font-extrabold transition duration-300 bg-green-400 rounded-3xl text-space1 hover:bg-space1 hover:text-space2 font-spacegrotesk hover:shadow-[0px_0px_20px_#7BE6D4">Our Event</a>
-            <a href="https://bit.ly/SALEGAMAFTR2024" className="px-6 py-2 font-extrabold transition duration-300 bg-green-400 rounded-3xl text-space1 hover:bg-space1 hover:text-space2 font-spacegrotesk hover:shadow-[0px_0px_20px_#31AFAB]">Order Ticket</a>
+            <a href="https://bit.ly/SALEGAMAFTR2024" className="px-6 py-2 font-extrabold transition duration-300 bg-green-600 rounded-3xl text-space1 hover:bg-space1 hover:text-space2 font-spacegrotesk hover:shadow-[0px_0px_20px_#31AFAB]">Order Ticket</a>
           </div>
 
           <div className="flex items-center md:hidden">
