de7135de1f90c566116984fc3bd65786887dc41a
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-18 23:49:46 +0700

    Update Info.jsx

--- gamaftr2025-main/src/app/components/Info.jsx
+++ gamaftr2025-main/src/app/components/Info.jsx
@@ -34,7 +34,7 @@ const Info = () => {
           <h1 className="mb-4 text-xl font-bold text-white font-pulsar md:text-xl sm:text-lg lg:text-4xl xl:text-6xl lg:text-border-space2">Gadjah Mada Flies To Riau</h1>
           <p className="font-semibold leading-relaxed text-justify text-white font-outfit md:text-md sm:text-sm lg:text-xl">Merupakan program kerja tahunan KEMARIGAMA (Keluarga Mahasiswa Riau Gadjah Mada) yang setiap tahunnya sudah mengunjungi lebih dari 50 SMA di Provinsi Riau. GAMAFTR diinisiasikan untuk memperkenalkan Universitas Gadjah Mada kepada siswa dan siswi SMA/sederajat di Provinsi Riau.</p>
         </div>
-        <img loading="lazy" className="absolute z-20 -translate-y-[30px] scale-[130%] -translate-x-3 w-[120%]  sm:scale-[120%] overflow-x-hidden" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/inikeretapi.png?raw=true" />
+        <img loading="lazy" className="absolute z-20 -translate-y-[30px] scale-[110%] -translate-x-3 w-[120%]  sm:scale-[100%] overflow-x-hidden" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/inikeretapi.png?raw=true" />
       </div>
      
     </div>
