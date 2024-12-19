4a4bb4ab238054957a8eb18afd5f6cadd0607a55
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-19 20:17:12 +0700

    Update Footer.jsx

--- gamaftr2025-main/src/app/components/Footer.jsx
+++ gamaftr2025-main/src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='relative md:mt-20 md:pt-36 bg-[#051d42]'>
           <div className="absolute z-10 left-[0%] bottom-[0%] scale-105 container mx-auto translate-y-1/5 ">
-           <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/sunandroad.png?raw=true" alt="Centered Image" className="mx-auto" /> </div>
+           <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/Frame%209.png?raw=true" alt="Centered Image" className="mx-auto" /> </div>
     <footer className="flex p-2 md:items-center sm:justify-between gap-[10px] md:gap-[40px] md:p-10 md:rounded-full shadow-2xl footer bg-space2/70 text-space1 shadow-red-700 md:scale-100 md:px-36 relative z-40">
   <aside className='flex'>
   <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/Gamaft/logogamaftr2025%20(2).png?raw=true" alt="" className="max-w-[60px] md:max-w-[150px] md:pl-4 pl-1 ml-3" />
