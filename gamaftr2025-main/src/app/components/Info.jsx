3bbd4b1fc52bfd1908818e5a6e2f7ff3081ba1fc
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-21 12:39:25 +0700

    Update Info.jsx

--- gamaftr2025-main/src/app/components/Info.jsx
+++ gamaftr2025-main/src/app/components/Info.jsx
@@ -9,7 +9,7 @@ const Info = () => {
           <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/Cloud.png?raw=true" alt="Alt Text" className='absolute z-10 rotate-180 -translate-y-[400px] sm:translate-x-10 translate-x-3'/>
         </div>
 
-        <div className="relative w-full p-4 translate-y-[200px]">
+        <div className="relative w-full bg-[#d3d3d3]/60  backdrop-blur-md p-4 translate-y-[200px]">
           <h1 className="relative z-40 text-xl font-bold text-center text-white rounded-2xl  md:text-2xl lg:text-4xl xl:text-6xl ">Gadjah Mada Flies To Riau</h1>
           <p className="relative z-40 leading-relaxed text-justify text-white font-outfit lg:text-2xl ">
           merupakan program kerja tahunan KEMARIGAMA (Keluarga Mahasiswa Riau Gadjah Mada) yang setiap tahunnya sudah mengunjungi lebih dari 50 SMA di Provinsi Riau. GAMAFTR diinsiasikan untuk memperkenalkan Universitas Gadjah Mada kepada siswa dan siswi SMA/sederajat di Provinsi Riau. GAMAFTR terdiri dari beberapa rangkaian acara seperti roadshow, try out, dan acara puncaknya adalah Bedah Kampus di Pekanbaru.
