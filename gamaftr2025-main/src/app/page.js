94b49d42efaa4b4cbda0b5f5340aa072d1facf51
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-21 22:03:11 +0700

    Update page.js

--- gamaftr2025-main/src/app/page.js
+++ gamaftr2025-main/src/app/page.js
@@ -51,7 +51,7 @@ const Home = () => {
   }, [controlsArray]);
 
   return (
-    <div className='p-1 overflow-x-hidden overflow-y-hidden bg-gradient-to-b from-blue-300 via-[#9D81E8] to-[#FD5289]'>
+    <div className='p-1 overflow-x-hidden overflow-y-hidden bg-gradient-to-b from-blue-300 via-[#9D81E8] to-[#ff8eb2]'>
       <Navbar />
 
       <AboutUsLg />
