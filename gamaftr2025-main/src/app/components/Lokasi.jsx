e496d9c3b7be839c21304151764861713b9f94da
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-21 12:51:50 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -5,13 +5,13 @@ const Lokasi = () => {
   return (
    <div className="relative z-30 flex justify-center mx-auto pt-36 md:mt-5 font-inter" id='event'>
     <div className="flex justify-center">
-    <div className="sm:border-[3px] sm:bg-[#d3d3d3]/60 backdrop-blur-md  sm:border-space2 sm:rounded-2x1  relative z-40 md:-translate-y-[100px] lg:-translate-y-[200px] max-w-[1280px]">
+    <div className="sm:border-[3px] sm:bg-[#d3d3d3]/60 backdrop-blur-md  sm:border-space2 sm:rounded-2x1 rounded-x1  relative z-40 md:-translate-y-[100px] lg:-translate-y-[200px] max-w-[1280px]">
     <div className="absolute z-40 right-[0%] bottom-[0%] -translate-x-1/2 sm:hidden translate-y-1/2">
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/batu%20angkasa.PNG?raw=true" alt="" className="" /> </div>
             <div className="absolute z-40 left-[0%] bottom-[0%] translate-x-1/3 sm:hidden translate-y-1/2">
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Group%2050.png?raw=true" alt="" className="" /> </div>
 
-    <h1 className="hidden sm:pt-10  text-3xl font-extrabold text-white s,:text-center sm:flex sm:justify-center font-spacegrotesk md:text-5xl text lg:text-7xl md:pt-5">RANGKAIAN ACARA</h1>
+    <h1 className="hidden sm:pt-10  text-3xl font-extrabold text-white s,:text-center sm:flex sm:justify-center rounded-x1 font-spacegrotesk md:text-5xl text lg:text-7xl md:pt-5">RANGKAIAN ACARA</h1>
     <div className="justify-center scale-75 xl:scale-75 sm:scale-[70%]  lg:scale-[60%] md:scale-[45%] md:-translate-y-[50px]  sm:-translate-y-[30px] sm:flex sm:flex-row-2 sm:gap-6 sm:grid-flow-col">
     <div className="md:w-1/2 md:px-8 sm:hidden scale-100 text-7xl font-spacegrotesk font-extrabold text-white rounded-2xl border-[4px] border-space2 bg-[#d3d3d3]/60 backdrop-blur-md  shadow-xl shadow-space2 xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40 text-border-space2 text-center">EVENTS</div>
     <div className="mt-5 transition shadow-xl card card-compact sm:w-1/2 w-50 md:w-70 lg:[50%] xl:[20%] bg-base-100 hover:scale-125 hover:z-40 hover:shadow-lg hover:shadow-space2 object-cover bg-gray-800">
