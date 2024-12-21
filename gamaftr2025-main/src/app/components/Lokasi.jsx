a66ed949efca5420d8630e3e50dfc075150f7567
Harum S Astyanti <hasatast@gmail.com>
2024-12-21 16:06:03 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -14,7 +14,7 @@ const Lokasi = () => {
     <h1 className="hidden sm:pt-10  text-3xl font-extrabold text-white s,:text-center sm:flex sm:justify-center rounded-x1 font-spacegrotesk md:text-5xl text lg:text-7xl md:pt-5">RANGKAIAN ACARA</h1>
     <div className="justify-center scale-75 xl:scale-75 sm:scale-[70%]  lg:scale-[60%] md:scale-[45%] md:-translate-y-[50px]  sm:-translate-y-[30px] sm:flex sm:flex-row-2 sm:gap-6 sm:grid-flow-col">
     <div className="md:w-1/2 md:px-8 sm:hidden scale-100 text-7xl font-spacegrotesk font-extrabold text-white rounded-2xl border-[4px] border-space2 bg-[#d3d3d3]/60 backdrop-blur-md  shadow-xl shadow-space2 xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40 text-border-space2 text-center">EVENTS</div>
-    <div className="mt-5 transition shadow-xl card card-compact sm:w-1/2 w-50 md:w-70 lg:[50%] xl:[20%] bg-base-100 hover:scale-125 hover:z-40 hover:shadow-lg hover:shadow-space2 object-cover bg-[#9D81E8]">
+    <div className="mt-5 transition shadow-xl card card-compact sm:w-1/2 w-50 md:w-70 lg:[50%] xl:[20%] hover:scale-125 hover:z-40 hover:shadow-lg hover:shadow-space2 object-cover bg-[#9D81E8]">
   <figure><img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/opening.png?raw=true" alt="Shoes" className='md:max-w-[350px]'  /></figure>
   <div className="card-body">
     <h2 className="font-bold card-title text-space2 font-spacegrotesk">Opening</h2>
