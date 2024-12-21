3075c820ddaf27f0a1ccca1ce563a8252bdbe6e4
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-21 22:07:27 +0700

    Update Ticket.jsx

--- gamaftr2025-main/src/app/components/Ticket.jsx
+++ gamaftr2025-main/src/app/components/Ticket.jsx
@@ -9,7 +9,7 @@ const Ticket = () => {
   return (
     <div className="hidden sm:block" id='ticket1'>
     <div className="flex flex-col sm:flex-row mx-auto min-h-screen items-center justify-center gap-4 xl:pt-60 md:pb-12 lg:my-[10px] relative">
-       <img loading="lazy" className="absolute z-10 lg:-translate-x-[40px] scale-[220%] 2xl:max-w-[1900px] md:translate-y-[300px]" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/Banner.png?raw=true" />
+       <img loading="lazy" className="absolute z-10 lg:-translate-x-[40px] scale-[220%] 2xl:max-w-[1900px] md:translate-y-[300px]" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/transparentbox.png?raw=true" />
 
 
       <div className="text-5xl font-spacegrotesk font-extrabold text-white rounded-2xl border-[4px]  bg-[#FFB0C5] shadow-xl  xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40  ">TICKET</div>
