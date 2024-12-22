4e16fb8de86b56333e59c3f2f37f17711a46e755
Harum S Astyanti <hasatast@gmail.com>
2024-12-22 15:33:56 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -28,8 +28,8 @@ const Lokasi = () => {
     <p className="py-4 font-outfit"> Simbolisasi pembukaan rangkaian kegiatan GAMAFTR 2025 sebagai langkah awal untuk menyapa seluruh siswa/i yang ada di Provinsi Riau.</p>
     <div className="mb-8">
             <h1 className="mb-4 text-3xl font-outfit font-bold">Event Information</h1>
-            <ul>
-                <li>class=font-outfit<strong>Lokasi : </strong> Car Free Day, Jl. Sudirman</li>
+            <ul className="font-outfit">
+                <li><strong>Lokasi : </strong> Car Free Day, Jl. Sudirman</li>
                 <li><strong>Tanggal : </strong> 5 Januari 2025</li>
                 <li><strong>Waktu : </strong> 07.00 - 09.00</li>
             </ul>
