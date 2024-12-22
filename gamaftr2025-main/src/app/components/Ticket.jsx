a004c9f1703045b34afb2e0d9638778c2130ed44
CarmiaKeiko <vaniacarmia@gmail.com>
2024-12-22 07:28:50 +0700

    Update Ticket.jsx

--- gamaftr2025-main/src/app/components/Ticket.jsx
+++ gamaftr2025-main/src/app/components/Ticket.jsx
@@ -25,7 +25,7 @@ const Ticket = () => {
           </div>
           {/* isi gamaftr ticket */}
           <div
-            className="absolute inset-0 h-full w-full rounded-xl bg-[#7BE6D4] p-8 [transform:rotateY(180deg)] [backface-visibility:hidden]  z-40" id='ticketweb'>
+            className="absolute inset-0 h-full w-full rounded-xl bg-[#FDD134] p-8 [transform:rotateY(180deg)] [backface-visibility:hidden]  z-40" id='ticketweb'>
             <div className="flex flex-col justify-between h-full ">
         <div className=" lg:mb-0 xl:mb-8 sm:mb-1 md:mb-1">
             <h1 className=" font-outfit font-bold sm:md:mb-0 lg:mb-0 xl:mb-4 text-[13px] md:text-[13px] sm:text-[8px] sm:mb-0  lg:text-2xl">Pekanbaru</h1>
