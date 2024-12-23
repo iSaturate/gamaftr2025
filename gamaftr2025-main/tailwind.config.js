02bc5577763008c400e7a45324b92c7f1a5101ff
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-23 12:06:37 +0700

    Update tailwind.config.js

--- gamaftr2025-main/tailwind.config.js
+++ gamaftr2025-main/tailwind.config.js
@@ -57,7 +57,7 @@ module.exports = {
         montserrat: ["var(--font-montserrat)"],
         spacegrotesk: ["var(--font-space-grotesk)"],
         outfit: ["var(--font-outfit)"],
-        novasquare: ["var(--font-nova-square)"],
+        audiowide: ["var(--font-audiowide)"],
 
       },
 
