2dc00d5c20a14447f83ed2e217f7c4862f95a446
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-23 20:52:49 +0700

    Update Info.jsx

--- gamaftr2025-main/src/app/components/Info.jsx
+++ gamaftr2025-main/src/app/components/Info.jsx
@@ -34,7 +34,7 @@ const Info = () => {
           <p className="font-semibold leading-relaxed text-justify text-white font-outfit md:text-md sm:text-sm lg:text-xl">Merupakan program kerja tahunan KEMARIGAMA (Keluarga Mahasiswa Riau Gadjah Mada) yang setiap tahunnya sudah mengunjungi lebih dari 50 SMA di Provinsi Riau. GAMAFTR diinisiasikan untuk memperkenalkan Universitas Gadjah Mada kepada siswa dan siswi SMA/sederajat di Provinsi Riau.</p>
         </div>
       </div>
-     <img loading="lazy" className="absolute z-20 -translate-y-[30px] scale-[120%] -translate-x-3  sm:scale-[140%] overflow-x-hidden" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/keretaapi.png?raw=true" />
+     <img loading="lazy" className="absolute z-20 -translate-y-[30px] scale-[80%] -translate-x-3  sm:scale-[100%] overflow-x-hidden" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/keretaapi.png?raw=true" />
     </div>
   );
 };
