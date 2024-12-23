5bef9ec3ca9a980ef25dd1c499ed3c2fa637188d
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-23 12:27:49 +0700

    Update Timeline.jsx

--- gamaftr2025-main/src/app/components/Timeline.jsx
+++ gamaftr2025-main/src/app/components/Timeline.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Timeline = () => {
   return (
     <div className="py-8 text-white  flex px-4 lg:pt-40 xl:pt-72 relative">
-      <div className="container flex-col items-start hidden mx-auto my-12 md:flex-row md:my-24 md:flex timeline md:-translate-x-[40px] lg:translate-x-[0px]">
+      <div className="container flex-col items-start hidden mx-auto my-12 md:flex-row md:my-24 md:flex timeline md:-translate-x-[50px] lg:translate-x-[20px]">
         <div className="relative flex flex-col items-center w-full px-8 mt-2 pt-36 md:top-36 lg:w-1/3 md:mt-12">
           <p className="relative z-40 flex justify-center p-2 text-xl md:text-2xl font-extrabold leading-normal  bg-[#8c8c8c]/20  shadow-white font-audiowide backdrop-blur-lg shadow-white temb-2 xl:text-6xl lg:text-4xl border-[4px] border-white md:leading-relaxed rounded-xl lg:text-border-white">TIMELINE</p>
             <img 
