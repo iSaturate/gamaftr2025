7c10a285070f725680cf7f0975ca3201a77f9160
maliknobelio <maliknobelio@gmail.com>
2024-12-23 15:46:17 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -24,7 +24,7 @@ const Lokasi = () => {
       <button className="border-none bg-[#FDD134] hover:bg-white hover:text-space2 text-gray-900 font-outfit btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>See All</button>
 <dialog id="my_modal_1" className="modal">
   <div className="modal-box">
-    <h3 className="md:text-[10px] xl:text-lg font-bold font-outfit text-yellow-200">Opening</h3>
+    <h3 className="md:text-[10px] xl:text-lg font-bold font-outfit text-[#FD5189]">Opening</h3>
     <p className="py-4 font-outfit"> Simbolisasi pembukaan rangkaian kegiatan GAMAFTR 2025 sebagai langkah awal untuk menyapa seluruh siswa/i yang ada di Provinsi Riau.</p>
     <div className="mb-8">
             <h1 className="mb-4 text-3xl font-outfit font-bold">Event Information</h1>
