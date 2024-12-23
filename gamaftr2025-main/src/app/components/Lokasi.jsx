c060af740329aa148123264d6b57a24ebbf5892c
maliknobelio <maliknobelio@gmail.com>
2024-12-23 17:11:57 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -34,7 +34,7 @@ const Lokasi = () => {
                 <li><strong>Waktu: </strong> 07.00 - 09.00</li>
             </ul>
         </div>
-    <div className="modal-action">
+    <div className="modal-action bg-space2">
       <form method="dialog">
         {/* if there is a button in form, it will close the modal */}
         <button className="font-outfit text-white">Close</button>
