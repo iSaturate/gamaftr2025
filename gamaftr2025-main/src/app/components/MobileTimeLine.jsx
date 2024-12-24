f70738d003300eecccde063f311fcb225d004867
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-24 11:36:06 +0700

    Update MobileTimeLine.jsx

--- gamaftr2025-main/src/app/components/MobileTimeLine.jsx
+++ gamaftr2025-main/src/app/components/MobileTimeLine.jsx
@@ -10,34 +10,34 @@ const MobileTimeline = () => {
           <div className="flex justify-center">
         
         </div>
-        <div className="text-5xl font-audiowide font-extrabold text-white bg-[#d3d3d3]/60 rounded-2xl border-[4px] border-white backdrop-blur-lg shadow-white temb-2 xl:text-8xl py-4 pl-10mr-[180px] relative z-40 text-border-white text-center">TIMELINE</div>
+        <div className="text-5xl font-audiowide font-bold text-white bg-[#d3d3d3]/60 rounded-2xl border-[4px] border-white backdrop-blur-lg shadow-white temb-2 xl:text-8xl py-4 pl-10mr-[180px] relative z-40 text-border-white text-center">TIMELINE</div>
         </div>
         <div className="relative z-40 md:w-1/2">
           <div className="container mx-auto">
             <div className="p-4 overflow-hidden">
               <div className="flex flex-col items-center justify-center mb-8 mobile-timeline-item">
-                <div className="w-1/2 px-1 py-2 text-center border-4 bg-space1 border-space2 rounded-3xl">
+                <div className="w-1/2 px-1 py-2 text-center border-4 bg-[#d3d3d3]/60 backdrop-blur-lg border-white rounded-3xl">
                   <p className="mb-3 text-lg font-bold text-[#FDD134] font-outfit md:text-2xl">5 Januari 2025</p>
                   <h4 className="mb-3 text-[#F1723D] font-extrabold font-outfit md:text-6xl">Opening</h4>
                 </div>
               </div>
 
               <div className="flex flex-col items-center justify-center mb-8 mobile-timeline-item">
-                <div className="w-1/2 px-1 py-2 text-center border-4 bg-space1 border-space2 rounded-3xl">
+                <div className="w-1/2 px-1 py-2 text-center border-4 bg-[#d3d3d3]/60 backdrop-blur-lg border-white rounded-3xl">
                   <p className="mb-3 text-lg font-bold text-[#FDD134] font-outfit md:text-2xl">6-17 Januari 2025</p>
                   <h4 className="mb-3 text-[#F1723D] font-extrabold font-outfit md:text-6xl">Roadshow</h4>
                 </div>
               </div>
 
               <div className="flex flex-col items-center justify-center mb-8 mobile-timeline-item">
-                <div className="w-1/2 px-1 py-2 text-center border-4 bg-space1 border-space2 rounded-3xl">
+                <div className="w-1/2 px-1 py-2 text-center border-4 bg-[#d3d3d3]/60 backdrop-blur-lg border-white rounded-3xl">
                   <p className="mb-3 text-lg font-bold text-[#FDD134] font-outfit md:text-2xl">18-19 Januari 2025</p>
                   <h4 className="mb-3 text-[#F1723D] font-extrabold font-outfit md:text-6xl">Try Out</h4>
                 </div>
               </div>
 
               <div className="flex flex-col items-center justify-center mb-8 mobile-timeline-item">
-                <div className="w-1/2 px-1 py-2 text-center border-4 bg-space1 border-space2 rounded-3xl">
+                <div className="w-1/2 px-1 py-2 text-center border-4 bg-[#d3d3d3]/60 backdrop-blur-lg border-white rounded-3xl">
                   <p className="mb-3 text-lg font-bold text-[#FDD134] font-outfit md:text-2xl">25 Januari 2025</p>
                   <h4 className="mb-3 text-[#F1723D] font-extrabold font-outfit md:text-6xl">Bedah Kampus</h4>
                 </div>
