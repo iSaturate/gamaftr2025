02288564faa643bef7d58389f569c8489ca4504e
CarmiaKeiko <vaniacarmia@gmail.com>
2024-12-24 23:09:13 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -17,7 +17,7 @@ const Lokasi = () => {
     <div className="mt-5 transition shadow-xl card card-compact sm:w-1/2 w-50 md:w-70 lg:[50%] xl:[20%] hover:scale-125 hover:z-40 hover:shadow-lg hover:shadow-space2 object-cover bg-space2">
   <figure><img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/opening.png?raw=true" alt="Shoes" className='md:max-w-[350px]'  /></figure>
   <div className="card-body">
-    <h2 className="font-bold card-title text-[#FDD134] font-outfit">Opening</h2>
+    <h2 className="font-bold card-title text-[#7BE6D4] font-outfit">Opening</h2>
      <p className='font-outfit text-xl text-white sm:text-sm md:text-sm'>Simbolisasi pembukaan rangkaian kegiatan GAMAFTR 2025</p>
     <div className="justify-end card-actions">
       <button className="border-none btn btn-primary bg-space1 backdrop-blur-lg hover:bg-white hover:text-space2  text-white font-outfit"><a href="https://maps.app.goo.gl/rEaZcotMjmZvzWAc8">Lokasi</a></button>
