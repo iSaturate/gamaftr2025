f9a2644e312c1f4a42d7b2bcf1615ca14762abf1
maliknobelio <maliknobelio@gmail.com>
2024-12-25 05:32:22 +0700

    Update Cardsmall.jsx

--- gamaftr2025-main/src/app/components/Cardsmall.jsx
+++ gamaftr2025-main/src/app/components/Cardsmall.jsx
@@ -5,24 +5,24 @@ const Cardsmall = () => {
   function handleClick() {
     window.location.href = "https://bit.ly/SALEGAMAFTR2024";
   }
-  
+
   return (
     <div className="sm:hidden" id='ticket1'>
-    <div className="flex flex-col sm:flex-row mx-auto min-h-screen items-center justify-center bg-space1 gap-4 xl:pt-60 md:pb-12 lg:my-[10px] relative">
-      <img loading="lazy" className="absolute z-10 lg:-translate-x-[40px] scale-[120%] 2xl:max-w-[1900px] md:translate-y-[300px]" alt="hero" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/transparentbox.png?raw=true" />
-     <div className="text-5xl font-spacegrotesk font-extrabold text-white rounded-2xl border-[4px] border-space2 bg-red-400 shadow-xl shadow-space2 xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40 text-border-space2 ">TICKET</div>
+    <div className="flex flex-col sm:flex-row mx-auto min-h-screen items-center justify-center bg-[#9D81EF] gap-4 xl:pt-60 md:pb-12 lg:my-[10px] relative">
+       <img loading="lazy" className="absolute z-30 lg:-translate-x-[40px] scale-[400%] max-w-[1220px] md:translate-y-[300px]" alt="hero" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/resize/awanasap%20(2)%20(1).png?raw=true" />
+
+
+      <div className="text-5xl font-spacegrotesk font-extrabold text-white rounded-2xl border-[4px] border-space2 bg-red-400 shadow-xl shadow-space2 xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40 text-border-space2 ">TICKET</div>
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40 ">
         <div
           className="relative w-full h-full transition-all duration-500 scale-90 shadow-xl">
           <div className="absolute inset-0 z-40 p-8 bg-gray-800 rounded-xl shadow-[0px_0px_20px_#FFC100]">
             <div className="flex flex-col justify-between h-full">
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/1704419489494.jpg?raw=true" alt="Shoes" className='md:max-w-[350px] rounded-xl shadow-[0px_0px_20px_#ff0000]' />
-              <h1 className="block pb-5 mt-auto text-4xl font-montserrat font-bold leading-5 text-[#FFC100]  xl:text-5xl">Try Out</h1>
+              <h1 className="block pb-5 mt-auto text-4xl font-outfit font-bold leading-5 text-[#FFC100]  xl:text-5xl">Try Out</h1>
               <div className="dropdown dropdown-right md:mt-2">
                 <button onClick={handleClick} className="border-3 text-space1 hover:hover:shadow-[0px_0px_20px_#FFC100] border-space2 hover:bg-space1 hover:text-space2 btn btn-primary bg-space2"><a className='font-bold font-spacegrotesk'>Daftar</a></button>
-            </div>
-          </div>
-                
+
   {/* <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
     <li><a href='https://daisyui.com/components/dropdown/'>Pekanbaru</a></li>
     <li><a>Duri</a></li>
@@ -54,5 +54,6 @@ const Cardsmall = () => {
     </div>
   );
 };
+
 
 export default Cardsmall;
