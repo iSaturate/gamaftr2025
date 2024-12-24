1197399cad879509f776d7908de1c7f23302fa2d
maliknobelio <maliknobelio@gmail.com>
2024-12-25 06:42:35 +0700

    Update Cardsmall.jsx

--- gamaftr2025-main/src/app/components/Cardsmall.jsx
+++ gamaftr2025-main/src/app/components/Cardsmall.jsx
@@ -9,9 +9,8 @@ const Cardsmall = () => {
   return (
     <div className="sm:hidden" id='ticket1'>
     <div className="flex flex-col sm:flex-row mx-auto min-h-screen items-center justify-center bg-[#FFFFFFF]/0 gap-4 xl:pt-60 md:pb-12 lg:my-[10px] relative">
-  
-
-
+      
+      {/* TO */}
       <div className="text-5xl font-audiowide font-extrabold text-white rounded-2xl border-[4px] border-white bg-[#d3d3d3]/40  shadow-xl xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40 ">TICKET</div>
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40 ">
         <div
@@ -21,24 +20,15 @@ const Cardsmall = () => {
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/1704419489494.jpg?raw=true" alt="Shoes" className='md:max-w-[350px] rounded-xl shadow-[0px_0px_20px_#F8F37B]' />
               <h1 className="block pb-5 mt-auto text-4xl font-outfit font-bold leading-5 text-space2  xl:text-5xl">Try Out</h1>
               <div className="dropdown dropdown-right md:mt-2">
-               
-
-  {/* <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-    <li><a href='https://daisyui.com/components/dropdown/'>Pekanbaru</a></li>
-    <li><a>Duri</a></li>
-    <li><a>Dumai</a></li>
-  </ul> */}
-</div>
+              </div>
             </div>
           </div>
-          {/* isi gamaftr ticket */}
         </div>
       </div>
 
-      {/* Bedah kampus */}
+      {/* Bedkam */}
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40">
-        <div
-          className="relative w-full h-full transition-all duration-500 scale-90 shadow-xl">
+        <div className="relative w-full h-full transition-all duration-500 scale-90 shadow-xl">
           <div className="absolute inset-0 z-40 p-8 bg-[#061328] rounded-xl">
             <div className="flex flex-col justify-between h-full">
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/1704419489494.jpg?raw=true" alt="Shoes" className='md:max-w-[350px] shadow-[0px_0px_20px_#F8F37B] rounded-xl' />
