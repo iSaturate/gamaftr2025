cd372976907fe9dcc6abc0feb975d04aa144e356
Calvin <46641531+iSaturate@users.noreply.github.com>
2024-12-24 11:25:40 +0700

    Update MobileTimeLine.jsx

--- gamaftr2025-main/src/app/components/MobileTimeLine.jsx
+++ gamaftr2025-main/src/app/components/MobileTimeLine.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const MobileTimeline = () => {
   return (
-    <div className="py-4 text-white bg-[#FFC100] md:hidden">
+    <div className="py-4 text-white md:hidden">
       <div className="container relative mx-auto my-12 md:flex-row md:my-24">
-      <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/image/marsbig.png?raw=true" alt="Alt Text" className='absolute z-10 translate-x-10 translate-y-20 md:hidden'/>
+      <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/mobiletrain.png?raw=true" alt="Alt Text" className='absolute z-10 flex justify-center translate-x-10 translate-y-20 md:hidden'/>
         <div className="px-[60PX] md:w-1/2 md:px-8 relative">
           <div className="flex justify-center">
         
         </div>
-        <div className="text-5xl font-audiowide font-extrabold text-white rounded-2xl border-[4px] border-white backdrop-blur-lg shadow-white temb-2 xl:text-8xl py-4 pl-10mr-[180px] relative z-40 text-border-white text-center">TIMELINE</div>
+        <div className="text-5xl font-audiowide font-extrabold text-white backdrop-blur-lg rounded-2xl border-[4px] border-white backdrop-blur-lg shadow-white temb-2 xl:text-8xl py-4 pl-10mr-[180px] relative z-40 text-border-white text-center">TIMELINE</div>
         </div>
         <div className="relative z-40 md:w-1/2">
           <div className="container mx-auto">
