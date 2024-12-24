86b470eb3ca03f00bc264a883478bdfd631ad68e
maliknobelio <maliknobelio@gmail.com>
2024-12-25 06:16:25 +0700

    Update Cardsmall.jsx

--- gamaftr2025-main/src/app/components/Cardsmall.jsx
+++ gamaftr2025-main/src/app/components/Cardsmall.jsx
@@ -16,7 +16,7 @@ const Cardsmall = () => {
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40 ">
         <div
           className="relative w-full h-full transition-all duration-500 scale-90 shadow-xl">
-          <div className="absolute inset-0 z-40 p-8 bg-[061328] rounded-xl">
+          <div className="absolute inset-0 z-40 p-8 bg-[#061328] rounded-xl">
             <div className="flex flex-col justify-between h-full">
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/1704419489494.jpg?raw=true" alt="Shoes" className='md:max-w-[350px] rounded-xl shadow-[0px_0px_20px_#F8F37B]' />
               <h1 className="block pb-5 mt-auto text-4xl font-outfit font-bold leading-5 text-space2  xl:text-5xl">Try Out</h1>
