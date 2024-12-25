21a86002835cf529573372cd771fa74d499dfd30
maliknobelio <maliknobelio@gmail.com>
2024-12-25 07:46:01 +0700

    Update Cardsmall.jsx

--- gamaftr2025-main/src/app/components/Cardsmall.jsx
+++ gamaftr2025-main/src/app/components/Cardsmall.jsx
@@ -25,16 +25,15 @@ const Cardsmall = () => {
         </div>
       </div>
 
-      
+      {/*bedkam*/}
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40">
         <div className="relative w-full h-full transition-all duration-500 scale-90 shadow-xl">
           <div className="absolute inset-0 z-40 p-8 bg-[#061328] rounded-xl">
             <div className="flex flex-col justify-between h-full">
             <img loading="lazy" src="https://github.com/SyaidFarhan/Gamaftr/blob/main/Gamaft/1704419489494.jpg?raw=true" alt="Shoes" className='md:max-w-[350px] shadow-[0px_0px_20px_#F8F37B] rounded-xl' />
               <h1 className="block pb-5 mt-auto text-4xl font-outfit font-bold text-space2  xl:text-5xl">Bedah Kampus</h1>
-              <div className="dropdown dropdown-right md:mt-2">
-                </div>
-              
+              <div className="dropdown dropdown-right md:mt-2"> 
+                
                 <div className="absolute z-40 inset-0 h-full w-full rounded-xl bg-space1  p-8 [transform:rotateY(180deg)] [backface-visibility:hidden]" id='ticketweb'>
                   <div className="flex flex-col justify-between h-full">
                     <div className=" lg:mb-0 xl:mb-8 sm:mb-1 md:mb-1">
@@ -44,14 +43,16 @@ const Cardsmall = () => {
                           <li className='text-[10px] sm:text-[8px]  md:text-[10px] lg:text-[14px]'><strong>Waktu pelaksanaan: </strong> 07.30 - Selesai</li>
                           <li className='text-[10px] sm:text-[8px] md:text-[10px] xl:text-[14px]'><strong>Lokasi: </strong>Menara Dang Merdu Bank Riau Kepri</li>
                         </ul>
-                      <div className="gap-4 mt-auto">
-                        <button onClick={handleClick} className="border-none text-white shadow-[0px_0px_20px_#205FC9] hover:bg-white hover:text-space2 hover: btn btn-primary bg-space2"><a className='font-bold font-outfit'>Daftar</a></button>
                       </div>
+                    <div className="gap-4 mt-auto">
+                      <button onClick={handleClick} className="border-none text-white shadow-[0px_0px_20px_#205FC9] hover:bg-white hover:text-space2 hover: btn btn-primary bg-space2"><a className='font-bold font-outfit'>Daftar</a></button>
                     </div>
                   </div>
                 </div>
-             </div>
-            </div>
+            
+              
+      </div>
+    </div>
               
   );
 };
