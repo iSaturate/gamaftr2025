11745de683bd94b40eea5d6063b1794803ce1de0
maliknobelio <maliknobelio@gmail.com>
2024-12-25 14:34:24 +0700

    Update Cardsmall.jsx

--- gamaftr2025-main/src/app/components/Cardsmall.jsx
+++ gamaftr2025-main/src/app/components/Cardsmall.jsx
@@ -10,7 +10,7 @@ const Cardsmall = () => {
     <div className="sm:hidden" id='ticket1'>
     <div className="flex flex-col sm:flex-row mx-auto min-h-screen items-center justify-center bg-[#FFFFFFF]/0 gap-4 xl:pt-60 md:pb-12 lg:my-[10px] relative">
 
-      <div className="text-5xl font-audiowide font-extrabold text-gray-300 bg-[#8c8c8c]/20 shadow-white rounded-2xl border-[4px] border-white backdrop-blur-lg lg:text-border-white backdrop-blur-lg shadow-xl xl:text-8xl p-4 lg:py-0 xl:p-4 pl-10mr-[180px] relative z-40" >TICKET</div>
+      <p className="relative z-40 flex justify-center p-2 text-xl md:text-3xl font-extrabold leading-normal  bg-[#8c8c8c]/20  shadow-white font-audiowide backdrop-blur-lg shadow-white temb-2 xl:text-6xl lg:text-5xl border-[4px] border-white md:leading-relaxed rounded-xl lg:text-border-white">TICKET</p>
       
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40 ">
         <div className="relative w-full h-full transition-all duration-500 scale-90 shadow-xl">
