a3bd321114113fe858ce8f7280f871cf5b92b8cf
maliknobelio <maliknobelio@gmail.com>
2024-12-25 21:54:53 +0700

    Update Sponsor.jsx

--- gamaftr2025-main/src/app/components/Sponsor.jsx
+++ gamaftr2025-main/src/app/components/Sponsor.jsx
@@ -45,7 +45,7 @@ const Sponsor = () => {
                 </div>
                
                     {/* medpart */}
-                    <div className="gap-5 px-2 pb-5 mb-2 text-center scale-[80%] bg-space2/40 sm:scale-100 rounded-2xl">
+                    <div className="gap-5 px-2 pb-5 mb-2 text-center scale-[80%] bg-white/40 sm:scale-100 rounded-2xl">
   <h1 className="flex items-center justify-center text-lg font-bold text-center font-audiowide text-space1 text-cyan-300">Media Partner</h1>
 
   {/* First Row */}
