e2a8a9f4e94b879a62d31ff75a772b16ef602798
CarmiaKeiko <vaniacarmia@gmail.com>
2024-12-25 14:55:25 +0700

    Update Cardsmall.jsx

--- gamaftr2025-main/src/app/components/Cardsmall.jsx
+++ gamaftr2025-main/src/app/components/Cardsmall.jsx
@@ -10,7 +10,7 @@ const Cardsmall = () => {
     <div className="sm:hidden" id='ticket1'>
     <div className="flex flex-col sm:flex-row mx-auto min-h-screen items-center justify-center bg-[#FFFFFFF]/0 gap-4 xl:pt-60 md:pb-12 lg:my-[10px] relative">
 
-      <div className="text-5xl font-audiowide font-bold text-gray-300 text-shadow-white bg-[#8c8c8c]/20 rounded-2xl border-[4px] border-white backdrop-blur-lg xl:text-5xl py-4 pl-10mr-[180px] relative z-40 text-center text-border-white">TICKET</div>
+      <div className="text-5xl font-audiowide font-bold text-gray-300 text-shadow-white bg-[#8c8c8c]/20 rounded-2xl border-[4px] border-white backdrop-blur-lg xl:text-8xl py-4 pl-10mr-[180px] relative z-40 text-center text-border-white"> TICKET </div>
 
       
       <div className="group h-96 w-80 xl:h-[575px] xl:w-[450px] [perspective:1000px] relative z-40 ">
