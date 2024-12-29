2b26c80c849f29440a8d198410dbf9ccb0cbcf5a
CarmiaKeiko <vaniacarmia@gmail.com>
2024-12-29 23:54:58 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -15,9 +15,9 @@ const Lokasi = () => {
     <div className="relative">
       <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/Group%201461.png?raw=true" alt="" className="translate-y-[-100px] translate-x-[300px] md:hidden w-[50%] h-[50%]" /> </div>
     <div className="relative">
-      <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/cylinder2.png?raw=true" alt="" className="translate-y-[50px] translate-x-[60px] md:hidden w-[35%] h-[35%]" /> </div>
+      <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/cylinder2.png?raw=true" alt="" className="rotate-[90deg] translate-y-[50px] translate-x-[60px] md:hidden w-[35%] h-[35%]" /> </div>
     <div className="relative">
-      <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/plane1.png?raw=true" alt="" className="rotate-[90deg] translate-y-[60px] translate-x-[80px] md:hidden w-[30%] h-[30%]" /> </div>
+      <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/plane1.png?raw=true" alt="" className="flip-[90deg] rotate-[60deg] translate-y-[60px] translate-x-[80px] md:hidden w-[30%] h-[30%]" /> </div>
     <div className="relative">
       <img loading="lazy" src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/CloudDouble.png?raw=true" alt="" className="translate-y-[50px] translate-x-[30px] md:hidden w-[1000%] h-auto" /> </div>
     
