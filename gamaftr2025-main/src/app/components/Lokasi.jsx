3aa622a55fa32a5789bdab00d47a496ea85d431a
Harum S Astyanti <hasatast@gmail.com>
2024-12-30 12:47:19 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -33,7 +33,7 @@ const Lokasi = () => {
       loading="lazy"
       src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/cylinder2.png?raw=true"
       alt=""
-      className="rotate-[100deg] translate-y-[180px] translate-x-[330px] md:hidden w-[60%] h-auto"
+      className="rotate-[100deg] translate-y-[180px] translate-x-[330px] md:hidden w-[57%] h-auto"
     />
   </div>
   <div>
@@ -41,7 +41,7 @@ const Lokasi = () => {
       loading="lazy"
       src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/cylinder2.png?raw=true"
       alt=""
-      className="rotate-[100deg] translate-y-[50px] translate-x-[230px] md:hidden w-[57%] h-auto"
+      className="rotate-[100deg] translate-y-[50px] translate-x-[230px] md:hidden w-[55%] h-auto"
     />
   </div>
   <div>
@@ -49,7 +49,7 @@ const Lokasi = () => {
       loading="lazy"
       src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/cylinder2.png?raw=true"
       alt=""
-      className="rotate-[100deg] translate-y-[-60px] translate-x-[130px] md:hidden w-[55%] h-auto"
+      className="rotate-[100deg] translate-y-[-60px] translate-x-[130px] md:hidden w-[52%] h-auto"
     />
   </div>
   <div>
@@ -57,7 +57,7 @@ const Lokasi = () => {
       loading="lazy"
       src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/plane1.png?raw=true"
       alt=""
-      className="rotate-[45deg] translate-y-[-290px] translate-x-[20px] md:hidden w-[50%] h-auto"
+      className="rotate-[45deg] translate-y-[-290px] translate-x-[20px] md:hidden w-[49%] h-auto"
     />
   </div>
   <div>
