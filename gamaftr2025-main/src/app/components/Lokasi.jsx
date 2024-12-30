0ed8ba03636d8e980d47e7ef9c391d8bf4ee57a0
Harum S Astyanti <hasatast@gmail.com>
2024-12-30 20:45:05 +0700

    Update Lokasi.jsx

--- gamaftr2025-main/src/app/components/Lokasi.jsx
+++ gamaftr2025-main/src/app/components/Lokasi.jsx
@@ -24,7 +24,7 @@ const Lokasi = () => {
       loading="lazy"
       src="https://github.com/iSaturate/gamaftrfoto/blob/main/gamaftrfoto/AwanSempurna2.png?raw=true"
       alt="hero"
-      className="absolute inset-0 z-[-1] translate-y-[30px] translate-x-[30px] md:hidden w-[100%] h-auto"
+      className="absolute inset-0 z-[-1] translate-y-[-30px] translate-x-[30px] md:hidden w-[100%] h-auto"
     />
   </div>
 </div>
